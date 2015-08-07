4a05c1acc163ebff40208f72ea5a04b3067a9d78
Marcus Vinicius P Azevedo <marcusvpazevedo@gmail.com>
2015-08-07 10:26:31 -0300

    SegundaVersao
    SegundaVersao

--- GFT_ConsoleApp_MarcusVPAzevedo/GFT_ConsoleApp_MarcusVPAzevedo/GFT_ConsoleApp_MarcusVPAzevedo/Program.cs
+++ GFT_ConsoleApp_MarcusVPAzevedo/GFT_ConsoleApp_MarcusVPAzevedo/GFT_ConsoleApp_MarcusVPAzevedo/Program.cs
@@ -5,225 +5,236 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 
-namespace GFT_ConsoleApp_MarcusVPAzevedo
+namespace GFT_ConsoleApp_MarcusVPAzevedo.TesteConsole
 {
-    static class Program
+    public class Program
     {
-        public static void Main(string[] args)
-        {
-            //o Unit Test não funcionou por algum motivo
-            //Fiz as chamadas por aqui para mostrar a execução, de acordo com as regras de negócio estabelecidas
-            string sParametros = "";
-            sParametros = "morning, 1, 1, 2, 2, 3, 3, 3, 4, 4, 5";
-            Executar(sParametros);
-            sParametros = "Morning, 2, 2, 1, 1, 3, 3, ";
-            Executar(sParametros);
-            sParametros = "Night, 1, 1, 2, 2, 2, 3, 3, 4, 4, 5";
-            Executar(sParametros);
-            sParametros = "nIght, 2, 2, 1, 1, 3, 3, ";
-            Executar(sParametros);
-            //
+        ClassificacaoPratos estrutura = new ClassificacaoPratos();
 
-            Menu();
+        public static void Main()
+        {
+            Program program = new Program();
+            program.Menu();
         }
 
-        public static void Executar(string sParametros)
+        public void ValidarExecutar(string sParametros)
         {
-            //trata o parâmetro, independente do conteúdo
-            sParametros = sParametros.Replace(" ", "").ToLower();
-            string sRetorno = "";
-            Retorno retorno = new Retorno();
-            StringBuilder sbErros = new StringBuilder();
+            SelecaoPratos retorno = new SelecaoPratos();
+            StringBuilder sbErrosParaConsole = new StringBuilder();
+            string sRetornoParaConsole = "";
+
+            sParametros = TratarParametros(sParametros);
 
             // Valida a entrada
             if (ValidarParametros(sParametros))
             {
-                List<Retorno> listaRetorno = MontarRetorno(sParametros, sbErros);
-
-                foreach (Retorno lRetorno in listaRetorno)
-                {
-                    sRetorno += lRetorno.NomeDish + ", ";
-                }
-
-                if (!sRetorno.Equals(""))
-                {
-                    sRetorno = sRetorno.Substring(0, sRetorno.Length - 2);
-                }
-
-                Console.WriteLine(sRetorno);
-
-                if (sbErros.Length > 0)
-                {
-                    string sErros = sbErros.ToString();
-                    sErros = sErros.Substring(0, sErros.Length - 2);
-                    sErros = "Alguns erros de entrada foram encontrados na sequência: " + sErros.ToString();
-                    Console.WriteLine(sErros);
-                    Console.WriteLine("\n");
-                }
-
+                List<SelecaoPratos> listaRetornos = MontarRetorno(sParametros, sbErrosParaConsole, ref sRetornoParaConsole);
+                MostrarResultados(sRetornoParaConsole, sbErrosParaConsole);
             }
         }
-        public static List<Retorno> MontarRetorno(string sParametros, StringBuilder sbErros)
+
+        private string TratarParametros(string sParametros)
         {
-            Retorno retorno = new Retorno();
-            List<Retorno> listaRetorno = new List<Retorno>();
-            string HoraDoDia = "";
+            //trata o parâmetro, independente do conteúdo
+            sParametros = sParametros.Replace(" ", "").ToLower();
+            return sParametros;
+        }
+        private bool ValidarParametros(string sParametros)
+        {            
+            bool bRetorno = true;
+            if (!sParametros.Trim().Equals(""))
+            {
+                string[] arrParametros = sParametros.Split(',');
+                if (arrParametros.Count() >= 4 && (arrParametros[0].Equals("morning") || arrParametros[0].Equals("night")))
+                { bRetorno = true; }
+                else 
+                { bRetorno = false; }
+            }
+            else { bRetorno = false; }
+
+            return bRetorno;
+        }
+
+        private List<SelecaoPratos> MontarRetorno(string sParametros, StringBuilder sbErros, ref string sRetornoParaConsole)
+        {
+            SelecaoPratos retorno = new SelecaoPratos();
+            List<SelecaoPratos> listaRetornos = new List<SelecaoPratos>();
+            string horaDoDia = "";
             string[] arrParametros = sParametros.Split(',');
 
-            int contador1 = 0;
-            int contador2 = 0;
-            int contador3 = 0;
-            int contador4 = 0;
-            
             //Analisa a posição 0, com a hora do dia
             retorno.Posicao = 0;
-            HoraDoDia = arrParametros[0];
-            retorno.NomeDish = HoraDoDia;
-            listaRetorno.Add(retorno);
+            horaDoDia = arrParametros[0];
+            retorno.NomeDish = horaDoDia;
+            listaRetornos.Add(retorno);
 
-            arrParametros = arrParametros.SubArray(1, arrParametros.Count() - 1); 
+            ClassificarPratos(arrParametros, sbErros);
+            SelecionarPratos(horaDoDia, listaRetornos);
+
+            foreach (SelecaoPratos lRetorno in listaRetornos)
+            {
+                sRetornoParaConsole += lRetorno.NomeDish + ", ";
+            }
+
+            if (!sRetornoParaConsole.Equals(""))
+            {
+                sRetornoParaConsole = sRetornoParaConsole.Substring(0, sRetornoParaConsole.Length - 2);
+            }
+
+            return listaRetornos;
+        }
+
+        private void MostrarResultados(string sRetornoParaConsole, StringBuilder sbErrosParaConsole)
+        {
+            Console.WriteLine(sRetornoParaConsole);
+
+            if (sbErrosParaConsole.Length > 0)
+            {
+                string sErros = sbErrosParaConsole.ToString();
+                sErros = sErros.Substring(0, sErros.Length - 2);
+                sErros = "Alguns erros de entrada foram encontrados na sequência: " + sErros.ToString();
+                Console.WriteLine(sErros);
+            }
+            Console.WriteLine("\n");
+        }
+        private void ClassificarPratos(string[] arrParametros, StringBuilder sbErros)
+        {
+            estrutura = new ClassificacaoPratos();
             //Analisa demais parâmetros, mesmo fora da ordem, em estrutura prevista (1 a 4)
             foreach (var param in arrParametros)
             {
-                if (param.Equals("1")) { contador1++; }
-                else if (param.Equals("2")) { contador2++; }
-                else if (param.Equals("3")) { contador3++; }
-                else if (param.Equals("4")) { contador4++; }
+                if (param.Equals("morning") || param.Equals("night")) { }
+                else if (param.Equals("1")) { estrutura.contador1++; }
+                else if (param.Equals("2")) { estrutura.contador2++; }
+                else if (param.Equals("3")) { estrutura.contador3++; }
+                else if (param.Equals("4")) { estrutura.contador4++; }
                 else
                 {
                     if (!param.Equals("")) { sbErros.AppendFormat("{0}, ", param); }
                 }
             }
+        }
+        private void SelecionarPratos(string horaDoDia, List<SelecaoPratos> listaRetorno)
+        {
+            SelecaoPratos retorno = new SelecaoPratos();
 
-            //Filtra parâmetros para montagem de retorno, usando Alias Dish, permitindo manutenção nas descrições
-            if (contador1 > 0)
+            //Filtra parâmetros para montagem de retorno
+            if (estrutura.contador1 > 0)
             {
-                retorno = new Retorno();
+                retorno = new SelecaoPratos();
                 retorno.Posicao = 1;
-                retorno.NomeDish = HoraDoDia.Equals("morning") ?
-                        GetDescription(Morning.Dish1) :
-                        GetDescription(Night.Dish1);
+                retorno.NomeDish = horaDoDia.Equals("morning") ? "Eggs" : "Steak";
                 listaRetorno.Add(retorno);
             }
-            if (contador2 > 0)
+            if (estrutura.contador2 > 0)
             {
-                retorno = new Retorno();
+                retorno = new SelecaoPratos();
                 retorno.Posicao = 2;
-                retorno.NomeDish = HoraDoDia.Equals("morning") ?
-                        GetDescription(Morning.Dish2) :
-                        GetDescription(Night.Dish2);
+                retorno.NomeDish = horaDoDia.Equals("morning") ? "Toast" : "Potato";
                 //multiplas potatoes para night
-                if (HoraDoDia.Equals("night"))
+                if (horaDoDia.Equals("night"))
                 {
-                    retorno.NomeDish += (contador2 > 1) ? ("(x" + contador2 + ")") : "";
+                    retorno.NomeDish += (estrutura.contador2 > 1) ? ("(x" + estrutura.contador2 + ")") : "";
                 }
                 listaRetorno.Add(retorno);
             }
-            if (contador3 > 0)
+            if (estrutura.contador3 > 0)
             {
-                retorno = new Retorno();
+                retorno = new SelecaoPratos();
                 retorno.Posicao = 3;
-                retorno.NomeDish = HoraDoDia.Equals("morning") ?
-                        GetDescription(Morning.Dish3) :
-                        GetDescription(Night.Dish3);
+                retorno.NomeDish = horaDoDia.Equals("morning") ? "Coffee" : "Wine";
                 //multiplos coffees para morning
-                if (HoraDoDia.Equals("morning"))
+                if (horaDoDia.Equals("morning"))
                 {
-                    retorno.NomeDish += (contador3 > 1) ? ("(x" + contador3 + ")") : "";
+                    retorno.NomeDish += (estrutura.contador3 > 1) ? ("(x" + estrutura.contador3 + ")") : "";
                 }
                 listaRetorno.Add(retorno);
             }
-            if (contador4 > 0)
+            if (estrutura.contador4 > 0)
             {
-                retorno = new Retorno();
+                retorno = new SelecaoPratos();
                 retorno.Posicao = 4;
-                retorno.NomeDish = HoraDoDia.Equals("morning") ?
-                        GetDescription(Morning.Dish4) :
-                        GetDescription(Night.Dish4);
+                retorno.NomeDish = horaDoDia.Equals("morning") ? "Error" : "Cake";
                 listaRetorno.Add(retorno);
             }
+        }
+        private class ClassificacaoPratos
+        {
+            private int _contador1;
+            private int _contador2;
+            private int _contador3;
+            private int _contador4;
 
-            return listaRetorno;
+            public int contador1
+            {
+                get { return _contador1; }
+                set { _contador1 = value; }
+            }
+            public int contador2
+            {
+                get { return _contador2; }
+                set { _contador2 = value; }
+            }
+            public int contador3
+            {
+                get { return _contador3; }
+                set { _contador3 = value; }
+            }
+            public int contador4
+            {
+                get { return _contador4; }
+                set { _contador4 = value; }
+            }
         }
-        public static T[] SubArray<T>(this T[] data, int index, int length)
+        private class SelecaoPratos
         {
-            T[] result = new T[length];
-            Array.Copy(data, index, result, 0, length);
-            return result;
-        }
-        public static bool ValidarParametros(string sParametros)
-        {
-            string[] arrParametros = sParametros.Split(',');
-            bool bRetorno = true;
-            if (!sParametros.Trim().Equals("") && arrParametros.Count() >= 4 &&
-                (arrParametros[0].Equals("morning") || arrParametros[0].Equals("night")))
-            { bRetorno = true; }
-            else { bRetorno = false; }
+            private int _Posicao;
+            private string _NomeDish;
 
-            return bRetorno;
-        }
-        public static string GetDescription(Enum value)
-        {
-            FieldInfo field = value.GetType().GetField(value.ToString());
-            DescriptionAttribute attribute =
-                Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) as DescriptionAttribute;
-
-            return attribute == null ? value.ToString() : attribute.Description;
-        }
-
-        public class Retorno
-        {
-            public int Posicao { get; set; }
-            public string NomeDish { get; set; }
-        }
-
-        public enum Morning
-        {
-            [Description("Eggs")]
-            Dish1 = 1,
-            [Description("Toast")]
-            Dish2 = 2,
-            [Description("Coffe")]
-            Dish3 = 3,
-            [Description("Error")]
-            Dish4 = 4
-        }
-        public enum Night
-        {
-            [Description("Steak")]
-            Dish1 = 1,
-            [Description("Potato")]
-            Dish2 = 2,
-            [Description("Wine")]
-            Dish3 = 3,
-            [Description("Cake")]
-            Dish4 = 4
+            public int Posicao
+            {
+                get { return _Posicao; }
+                set { _Posicao = value; }
+            }
+            public string NomeDish
+            {
+                get { return _NomeDish; }
+                set { _NomeDish = value; }
+            }
         }
 
-        public static void Menu()
-        {
-            ConfigurarCorDaFonte(ConsoleColor.Gray);
-            Console.WriteLine("Para sair, tecle S");
-
-            OpcaoSelecionada();
-        }
-        public static void ConfigurarCorDaFonte(ConsoleColor corDaFonte)
+        private void ConfigurarCorDaFonte(ConsoleColor corDaFonte)
         {
             Console.ForegroundColor = corDaFonte;
         }
-        public static void OpcaoSelecionada()
+        private void OpcaoSelecionada()
         {
             ConsoleKeyInfo keyInfo = new ConsoleKeyInfo();
             keyInfo = Console.ReadKey(true);
 
-            if (keyInfo.Key == ConsoleKey.S)
-            {
-                Console.Beep();
-                return;
-            }
+            if (keyInfo.Key == ConsoleKey.A) { ValidarExecutar("Morning, 1, 2, 3"); }
+            if (keyInfo.Key == ConsoleKey.B) { ValidarExecutar("Morning, 2, 1, 3"); }
+            if (keyInfo.Key == ConsoleKey.C) { ValidarExecutar("Morning, 1, 2, 3, 4"); }
+            if (keyInfo.Key == ConsoleKey.D) { ValidarExecutar("Morning, 1, 2, 3, 3, 3"); }
+            if (keyInfo.Key == ConsoleKey.E) { ValidarExecutar("Night, 1, 2, 3, 4"); }
+            if (keyInfo.Key == ConsoleKey.F) { ValidarExecutar("Night, 1, 2, 2, 4"); }
+            if (keyInfo.Key == ConsoleKey.S) { return; }
 
             Menu();
         }
+        private void Menu()
+        {
+            ConfigurarCorDaFonte(ConsoleColor.Gray);
+            Console.WriteLine("Para opção de Menu: 'Morning', 1, 2, 3, tecle 1");
+            Console.WriteLine("Para opção de Menu: 'Morning', 2, 1, 3, tecle 2");
+            Console.WriteLine("Para opção de Menu: 'Morning', 1, 2, 3, 4, tecle 3");
+            Console.WriteLine("Para opção de Menu: 'Morning', 1, 2, 3, 3, 3, tecle 4");
+            Console.WriteLine("Para opção de Menu: 'Night', 1, 2, 3, 4, tecle 5");
+            Console.WriteLine("Para opção de Menu: 'Night', 1, 2, 2, 4, tecle 6");
+            Console.WriteLine("Para sair, tecle S");
+            Console.WriteLine("\n");
 
+            OpcaoSelecionada();
+        }
     }
 }
--- GFT_ConsoleApp_MarcusVPAzevedo/GFT_ConsoleApp_MarcusVPAzevedo/UnitTestProject/UnitTest.cs
+++ GFT_ConsoleApp_MarcusVPAzevedo/GFT_ConsoleApp_MarcusVPAzevedo/UnitTestProject/UnitTest.cs
@@ -1,16 +1,64 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using GFT_ConsoleApp_MarcusVPAzevedo;
+﻿using NUnit.Framework;
+using GFT_ConsoleApp_MarcusVPAzevedo.TesteConsole;
 
 namespace UnitTestProject
 {
-    [TestClass]
+    [TestFixture]
     public class UnitTest
     {
-        [TestMethod]
-        public void TesConsole()
+        [Test]
+        public void TesteConsoleSample1()
         {
-
+            GFT_ConsoleApp_MarcusVPAzevedo.TesteConsole.Program gft;
+            gft = new GFT_ConsoleApp_MarcusVPAzevedo.TesteConsole.Program();
+            //Input: morning, 1, 2, 3
+            //Output: eggs, toast, coffee
+            gft.ValidarExecutar("morning, 1, 2, 3");
+        }
+        [Test]
+        public void TesteConsoleSample2()
+        {
+            GFT_ConsoleApp_MarcusVPAzevedo.TesteConsole.Program gft;
+            gft = new GFT_ConsoleApp_MarcusVPAzevedo.TesteConsole.Program();
+            //Input: morning, 2, 1, 3
+            //Output: eggs, toast, coffee
+            gft.ValidarExecutar("morning, 2, 1, 3");
+        }
+        [Test]
+        public void TesteConsoleSample3()
+        {
+            GFT_ConsoleApp_MarcusVPAzevedo.TesteConsole.Program gft;
+            gft = new GFT_ConsoleApp_MarcusVPAzevedo.TesteConsole.Program();
+            //Input: morning, 1, 2, 3, 4
+            //Output: eggs, toast, coffee, error
+            gft.ValidarExecutar("morning, 1, 2, 3, 4");
+        }
+        [Test]
+        public void TesteConsoleSample4()
+        {
+            GFT_ConsoleApp_MarcusVPAzevedo.TesteConsole.Program gft;
+            gft = new GFT_ConsoleApp_MarcusVPAzevedo.TesteConsole.Program();
+            //Input: morning, 1, 2, 3, 3, 3
+            //Output: eggs, toast, coffee(x3)
+            gft.ValidarExecutar("morning, 1, 2, 3, 3, 3");
+        }
+        [Test]
+        public void TesteConsoleSample5()
+        {
+            GFT_ConsoleApp_MarcusVPAzevedo.TesteConsole.Program gft;
+            gft = new GFT_ConsoleApp_MarcusVPAzevedo.TesteConsole.Program();
+            //Input: night, 1, 2, 3, 4
+            //Output:  steak, potato, wine, cake
+            gft.ValidarExecutar("night, 1, 2, 3, 4");
+        }
+        [Test]
+        public void TesteConsoleSample6()
+        {
+            GFT_ConsoleApp_MarcusVPAzevedo.TesteConsole.Program gft;
+            gft = new GFT_ConsoleApp_MarcusVPAzevedo.TesteConsole.Program();
+            //Input: night, 1, 2, 2, 4
+            //Output steak, potato(x2), cake
+            gft.ValidarExecutar("night, 1, 2, 2, 4");
         }
     }
 }
